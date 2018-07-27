be561f03a1c04f2d7bcdc354db69234cd77721f0
Christian <christian.koehler@3imedia.de>
2018-07-27 11:31:15 +0200

    Added field for Working Directory

--- src/changelog-generator/task/changelogGenerator.ps1
+++ src/changelog-generator/task/changelogGenerator.ps1
@@ -4,7 +4,9 @@ try {
     #$global: __vstsNoOverrideVerbose = $true
 
     # Get inputs from TFS/VSTS
+    $source = Get-VstsInput -Name "sourcePath" -Require -ErrorAction "Stop"
     $filename = Get-VstsInput -Name "filename" -Require -ErrorAction "Stop"
+    Set-Location $source
     git log --oneline  --date=format:"%Y-%m-%d %H:%M" --pretty=format "- %cd %h %s %d <%ce>" --no-merges > $filename
 } catch {
     Write-Verbose "Exception caught from task: $($_.Exception.ToString())"
