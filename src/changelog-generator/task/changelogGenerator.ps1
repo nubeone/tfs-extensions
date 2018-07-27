6da90f4b9b2dddfa1c0e6d0d8d4e51bf2451f805
Ali, Muhannad (NubeOne) <muhannad.ali@nubeone.de>
2018-07-27 11:47:58 +0200

    rename source path in working directory

--- src/changelog-generator/task/changelogGenerator.ps1
+++ src/changelog-generator/task/changelogGenerator.ps1
@@ -4,10 +4,10 @@ try {
     #$global: __vstsNoOverrideVerbose = $true
 
     # Get inputs from TFS/VSTS
-    $source = Get-VstsInput -Name "sourcePath" -Require -ErrorAction "Stop"
+    $workingDir = Get-VstsInput -Name "workingDir" -Require -ErrorAction "Stop"
     $filename = Get-VstsInput -Name "filename" -Require -ErrorAction "Stop"
-    Set-Location $source
-    git log --oneline  --date=format:"%Y-%m-%d %H:%M" --pretty=format "- %cd %h %s %d <%ce>" --no-merges > $filename
+    Set-Location $workingDir
+    git log --oneline  --date=format:"%Y-%m-%d %H:%M" --pretty=format:"- %cd %h %s %d <%ce>" --no-merges > $filename
 } catch {
     Write-Verbose "Exception caught from task: $($_.Exception.ToString())"
     throw
