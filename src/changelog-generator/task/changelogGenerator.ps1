fb05d6e1df7961797b206879ed94409845dafb63
Christian <christian.koehler@3imedia.de>
2018-07-30 10:43:53 +0200

    changed icon, added buildnumber to file

--- src/changelog-generator/task/changelogGenerator.ps1
+++ src/changelog-generator/task/changelogGenerator.ps1
@@ -8,12 +8,13 @@ try {
     $filename = Get-VstsInput -Name "filename" -Require -ErrorAction "Stop"
     $commits = Get-VstsInput -Name "commits" -Require -ErrorAction "Stop"
     Set-Location $workingDir
+    "#v#{Build.BuildNumber}#" > $filename
     if($commits -eq "true") {
-        git log --oneline  --date=format:"%Y-%m-%d %H:%M" --pretty=format:"- %cd %h %s %d <%ce>" --no-merges > $filename
+        git log --oneline --date=format:"%Y-%m-%d %H:%M" --pretty=format:"- %cd %h %s %d <%ce>" --no-merges >> $filename
     } else {
         $tag = git describe --tags --abbrev=0
         $tag = $tag + "..@"
-        git log --oneline $tag --date=format:\"%Y-%m-%d %H:%M\" --pretty=format:\""+prettyFormat+"\" --no-merges > $filename
+        git log --oneline $tag --date=format:"%Y-%m-%d %H:%M" --pretty=format:"- %cd %h %s %d <%ce>" --no-merges >> $filename
     }
 } catch {
     Write-Verbose "Exception caught from task: $($_.Exception.ToString())"
